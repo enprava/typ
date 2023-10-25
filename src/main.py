877cf0a6a69062e0802b724ea5eb67594a28fe1a
Edugonzaper <eddugonzzaperrez@gmail.com>
2023-10-25 14:25:06 +0200

    Archivos principales

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
-from selenium import webdriver
+# # from selenium import webdriver
 from selenium.webdriver.common.by import By
 from utils import *
+import itertools
+#
+# driver = webdriver.Firefox()
+# driver.get('http://trackwine.indexageodata.com/')
+# acciones = [[By.CLASS_NAME, 'welcome']]
+# print(apply_actions(driver, acciones))
+# driver.close()
 
-driver = webdriver.Firefox()
-driver.get('http://trackwine.indexageodata.com/')
-acciones = [[By.CLASS_NAME, 'welcome']]
-print(apply_actions(driver, acciones))
-driver.close()
+
+npaginas = range(1,3)
+
+paginas = get_pages("https://www.destacamos.com/3-travestis/{}/listings.html",npaginas)
+drivers = get_drivers(paginas)
+
+nodes = get_nodes_from_driver(drivers,[By.CLASS_NAME,"titulo"])
+for node in nodes:
+    node = list(map(lambda x: apply_action(x, [By.TAG_NAME,"a"]),node))
+    print(node)
+    hrefs = get_href_from_nodes(node)
+    print(hrefs)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import os
+import requests
+from selenium.webdriver.common.by import By
+import pickle
+# # from selenium.webdriver.common.by import By
+
 def apply_action(driver, action):
     return driver.find_element(action[0], action[1])
 
 def apply_actions(driver, actions):
-    return list(map(lambda x: apply_action(driver, x), actions))
+    for action in actions:
+        driver = apply_action(driver, action)
+    return driver
+
+def get_pages(url,n_pages):
+    return list(map(lambda x: url.format(x), n_pages))
+
+def init_driver(url):
+    options = Options()
+    options.add_argument("--headless")
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    return driver
+def get_drivers(paginas):
+
+    return list(map(lambda x: init_driver(x), paginas))
+
+def get_nodes_from_driver(drivers,action):
+    return list(map(lambda x: x.find_elements(action[0],action[1]),drivers))
+def get_href_from_nodes(nodes):
+
+    return list(map(lambda x: x.get_attribute("href"), nodes))
+
+def get_imgs_from_node(node,pag_path):
+
+    if not os.path.exists(os.path.join(pag_path, "imagenes")):
+        os.makedirs(os.path.join(pag_path, "imagenes"))
+
+    img_tags = node.find_elements(By.TAG_NAME, "img")
+    for img_tag in img_tags:
+        img_url = img_tag.get_attribute("src")
+
+        img_name = os.path.join(pag_path, "imagenes", os.path.basename(img_url))
+
+        try:
+            response = requests.get(img_url)
+            if response.status_code == 200:
+                with open(img_name, 'wb') as img_file:
+                    img_file.write(response.content)
+
+        except:
+            print("Error al descargar la imagen")
+
+
+def serialize_pickle_node(node,path,file_name):
+    with open(os.path.join(path, file_name), 'wb') as pickle_file:
+        pickle.dump(node.page_source, pickle_file
+
+def deserialize_pickle_node(path):
+    with open(path, 'rb') as archivo:
+        html_content = pickle.load(archivo)
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+        return soup
+
