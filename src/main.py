6fb16156bc7be76d6019cfbf7513353b6233f072
enprava <epradavazquez@gmail.com>
2023-10-26 09:47:57 +0200

    Update requirements.txt, main.py, and utils.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
 from selenium.webdriver.common.by import By
 from utils import *
+import time
 
-
-
-npaginas = range(1,3)
+npaginas = range(1)
 
 paginas = get_pages("https://www.destacamos.com/3-travestis/{}/listings.html",npaginas)
 drivers = get_drivers(paginas)
 
 nodes = get_nodes_from_driver(drivers,[By.CLASS_NAME,"titulo"])
-for node in nodes:
-    node = list(map(lambda x: apply_action(x, [By.TAG_NAME,"a"]),node))
-    print(node)
-    hrefs = get_href_from_nodes(node)
-    print(hrefs)
+
+nodes = list(map(lambda x: list(map(lambda y: apply_action(y, [By.TAG_NAME,"a"]), x)), nodes))
+hrefs = list(map(lambda x: get_href_from_nodes(x), nodes))
+shows = list(map(lambda x: get_drivers(x), hrefs))
+
--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,8 @@ import os
 import requests
 from selenium.webdriver.common.by import By
 import pickle
-# # from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
 
 def apply_action(driver, action):
     return driver.find_element(action[0], action[1])
@@ -14,8 +15,10 @@ def apply_actions(driver, actions):
         driver = apply_action(driver, action)
     return driver
 
-def get_pages(url,n_pages):
+
+def get_pages(url, n_pages):
     return list(map(lambda x: url.format(x), n_pages))
+
 
 def init_driver(url):
     options = Options()
@@ -23,18 +26,21 @@ def init_driver(url):
     driver = webdriver.Chrome(options=options)
     driver.get(url)
     return driver
-def get_drivers(paginas):
 
+
+def get_drivers(paginas):
     return list(map(lambda x: init_driver(x), paginas))
 
-def get_nodes_from_driver(drivers,action):
-    return list(map(lambda x: x.find_elements(action[0],action[1]),drivers))
-def get_href_from_nodes(nodes):
 
+def get_nodes_from_driver(drivers, action):
+    return list(map(lambda x: x.find_elements(action[0], action[1]), drivers))
+
+
+def get_href_from_nodes(nodes):
     return list(map(lambda x: x.get_attribute("href"), nodes))
 
-def get_imgs_from_node(node,pag_path):
 
+def get_imgs_from_node(node, pag_path):
     if not os.path.exists(os.path.join(pag_path, "imagenes")):
         os.makedirs(os.path.join(pag_path, "imagenes"))
 
@@ -47,21 +53,28 @@ def get_imgs_from_node(node,pag_path):
         try:
             response = requests.get(img_url)
             if response.status_code == 200:
-                with open(img_name, 'wb') as img_file:
+                with open(img_name, "wb") as img_file:
                     img_file.write(response.content)
 
         except:
             print("Error al descargar la imagen")
 
 
-def serialize_pickle_node(node,path,file_name):
-    with open(os.path.join(path, file_name), 'wb') as pickle_file:
+def serialize_pickle_node(node, path, file_name):
+    with open(os.path.join(path, file_name), "wb") as pickle_file:
         pickle.dump(node.page_source, pickle_file)
 
-def deserialize_pickle_node(path):
-    with open(path, 'rb') as archivo:
-        html_content = pickle.load(archivo)
 
-        soup = BeautifulSoup(html_content, 'html.parser')
+def deserialize_pickle_node(path):
+    with open(path, "rb") as archivo:
+        html_content = pickle.load(archivo)
+        soup = BeautifulSoup(html_content, "html.parser")
         return soup
 
+
+def do_click(driver, actions):
+    apply_actions(driver, actions).click()
+
+
+def scroll_down(driver):
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
